308b351338b7fa46b32dfe3d0353a2385081bb0d
Jay <Jay@Athena.local>
2019-08-31 16:39:41 +0000

    var, let & const

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-alert('Hello World');
+// PRIMITIVE
+
+// String
+
+const name = 'John Doe';
+console.log(typeof name);
+
+// Number
+
+const age = 45;
+
+// Boolean
+
+const hasKids = true;
+
+// Null
+
+const car = null;
+
+// Undefined
+
+let test; // const will be an error cause you have to give it value
+
+// Symbol
+
+const sym = Symbol();
+
+// REFERENCE TYPES - Objects
+// Array
+
+const hobbies = ['movies', 'music'];
+
+// Object Literal
+
+const address = {
+  city: 'Boston',
+  state: 'MA',
+};
---
+++ app1.js
@@ -0,0 +1,42 @@
+// Log to console
+
+console.log('Hello World');
+console.log(123); // numbers
+console.log(true); // Boolean
+
+let greeting = 'Hello'; // Variables
+console.log(greeting);
+
+console.log([1, 2, 3, 4]); // Arrays
+console.log({ a: 1, b: 2, c: 3 }); // Objects Literal
+console.table({ a: 1, b: 2, c: 3 }); // Prints objects as table
+
+console.error('This is an error'); // Prints error
+console.warn('This is a warning'); // Prints warning
+console.clear(); // Clears console
+
+console.time('Hello');
+
+// If you want to know how long something taks to run. Put inbetween
+
+console.timeEnd('Hello');
+
+// var, let, const
+
+var name = ('John Doe');
+console.log(name);
+name = 'Steve Smith';
+console.log('name');
+
+// Initialize Var
+var greeting;
+greeting = 'Hello';
+
+// Rules of varaibles | letters, numbers, _, $
+// Can not start with numbers
+
+// Multi word varaibles
+
+var firstName = 'John'; // camel case
+var FIRST_NAME = 'Sara'; // Underscore
+var FirstName = 'Tom'; // Pascal case
